4039822d86b0aeb34949bf144981074e9e17492d
Leon-Sander <leon.sander@protonmail.com>
2024-02-13 11:58:01 +0100

    License update

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+# Local Multimodal AI Chat - Multimodal chat application with local models
+# Copyright (C) 2024 Leon Sander
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 import streamlit as st
 from llm_chains import load_normal_chain, load_pdf_chat_chain
 from streamlit_mic_recorder import mic_recorder
